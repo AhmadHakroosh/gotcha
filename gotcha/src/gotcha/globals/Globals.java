3574c30fb771dc86686a6f6d367c58f543d329f1
Ahmad Hakroosh <ahmad@ahmad-h.com>
2017-01-03 18:11:54 +0200

    Global interface implementation (Database related)

--- gotcha/src/gotcha/globals/Globals.java
+++ gotcha/src/gotcha/globals/Globals.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
@@ -27,15 +28,44 @@ public interface Globals {
 					connection = DriverManager.getConnection(protocol + dbName + ";create=true");
 					Statement statement = connection.createStatement();
 					// Add the tables
-					statement.executeUpdate("CREATE TABLE USERS (NAME VARCHAR(10) NOT NULL PRIMARY KEY,PASSWORD VARCHAR(8) NOT NULL,NICKNAME VARCHAR(20) UNIQUE,DESCRIPTION VARCHAR(50),PHOTO VARCHAR(512));");
+					statement.executeUpdate("CREATE TABLE USERS ("
+											+ "NAME VARCHAR(10) NOT NULL PRIMARY KEY,"
+											+ "PASSWORD VARCHAR(8) NOT NULL,"
+											+ "NICKNAME VARCHAR(20) UNIQUE,"
+											+ "DESCRIPTION VARCHAR(50),"
+											+ "PHOTO VARCHAR(512)"
+											+ ");"
+											);
 	        		 
-	        		statement.executeUpdate("CREATE TABLE CHANNELS (NAME VARCHAR(40) PRIMARY KEY,DESCRIPTION VARCHAR(100) NOT NULL,SUBSCRIBERS INTEGER);");
+	        		statement.executeUpdate("CREATE TABLE CHANNELS ("
+					        				+ "NAME VARCHAR(40) PRIMARY KEY,"
+					        				+ "DESCRIPTION VARCHAR(100) NOT NULL,"
+					        				+ "SUBSCRIBERS INTEGER"
+					        				+ ");"
+					        				);
 
-	        		statement.executeUpdate("CREATE TABLE SUBSCRIPTIONS (ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,USERNAME VARCHAR(10) NOT NULL REFERENCES APP.USERS(NAME) ON DELETE CASCADE,CHANNEL VARCHAR(40) NOT NULL REFERENCES APP.CHANNELS(NAME) ON DELETE CASCADE);");
+	        		statement.executeUpdate("CREATE TABLE SUBSCRIPTIONS ("
+	        								+ "ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,"
+	        								+ "USERNAME VARCHAR(10) NOT NULL REFERENCES APP.USERS(NAME) ON DELETE CASCADE,"
+	        								+ "CHANNEL VARCHAR(40) NOT NULL REFERENCES APP.CHANNELS(NAME) ON DELETE CASCADE"
+	        								+ ");"
+	        								);
 
-	        		statement.executeUpdate("CREATE TABLE MESSAGE (ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,SENT TIMESTAMP NOT NULL,TEXT VARCHAR(2500) NOT NULL);");
+	        		statement.executeUpdate("CREATE TABLE MESSAGE ("
+	        								+ "ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,"
+	        								+ "SENT TIMESTAMP NOT NULL,"
+	        								+ "TEXT VARCHAR(2500) NOT NULL"
+	        								+ ");"
+	        								);
 
-	        		statement.executeUpdate("CREATE TABLE MESSAGES (ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,SENDER VARCHAR(10) NOT NULL REFERENCES APP.USERS(NAME) ON DELETE CASCADE,RECEIVER VARCHAR(10) NOT NULL REFERENCES APP.USERS(NAME) ON DELETE CASCADE,CHANNEL VARCHAR(40) NOT NULL REFERENCES APP.CHANNELS(NAME) ON DELETE CASCADE,MESSAGE INTEGER NOT NULL REFERENCES APP.MESSAGE(ID) ON DELETE CASCADE);");
+	        		statement.executeUpdate("CREATE TABLE MESSAGES ("
+	        								+ "ID INTEGER NOT NULL GENERATED ALWAYS AS IDENTITY (START WITH 1, INCREMENT BY 1) PRIMARY KEY,"
+	        								+ "SENDER VARCHAR(10) NOT NULL REFERENCES APP.USERS(NAME) ON DELETE CASCADE,"
+	        								+ "RECEIVER VARCHAR(10) NOT NULL REFERENCES APP.USERS(NAME) ON DELETE CASCADE,"
+	        								+ "CHANNEL VARCHAR(40) NOT NULL REFERENCES APP.CHANNELS(NAME) ON DELETE CASCADE,"
+	        								+ "MESSAGE INTEGER NOT NULL REFERENCES APP.MESSAGE(ID) ON DELETE CASCADE"
+	        								+ ");"
+	        								);
 					// Commit the changes
 	        		connection.commit();
 	        		statement.close();
@@ -59,10 +89,12 @@ public interface Globals {
 			return connection;
 		}
 		
-		public static boolean insertInto (String table, ArrayList<String> values) {
+		public static void execute (String query) throws SQLException {
 			Connection connection = getConnection();
-			
-			return true;
+			Statement statement = connection.createStatement();
+			ResultSet resultSet = statement.executeQuery(query);
+			System.out.println(resultSet.toString());
+			statement.close();
 		}
 	}
 }
