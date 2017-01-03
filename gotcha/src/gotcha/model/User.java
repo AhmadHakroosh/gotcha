33bb428e65ffa4040507b9da6f0fd74d38c10168
Ahmad Hakroosh <ahmad@ahmad-h.com>
2017-01-03 18:41:40 +0200

    Implemented User class

---
+++ gotcha/src/gotcha/model/User.java
@@ -0,0 +1,70 @@
+package gotcha.model;
+
+import java.util.HashMap;
+import java.util.Map.Entry;
+
+public class User {
+	// User Attributes
+	private String name;
+	private String password;
+	private String nickName;
+	private String description;
+	private String photoUrl;
+	
+	// Default constructor
+	public User () {
+		// Do nothing...
+	}
+	
+	public User (String name, String password) {
+		this.name = name;
+		this.password = password;
+	}
+
+	public User (String name, String password, String nickName) {
+		this.name = name;
+		this.password = password;
+		this.nickName = nickName;
+	}
+	
+	public User (String name, String password, String nickName, String description) {
+		this.name = name;
+		this.password = password;
+		this.nickName = nickName;
+		this.description = description;
+	}
+
+	public User (String name, String password, String nickName, String description, String photoUrl) {
+		this.name = name;
+		this.password = password;
+		this.nickName = nickName;
+		this.description = description;
+		this.photoUrl = photoUrl;
+	}
+	// User authentication method
+	public boolean authenticate (String password) {
+		return password.equals(this.password) ? true : false;
+	}
+	
+	public void update (HashMap<String, String> attributes) {
+		for (Entry<String, String> attribute : attributes.entrySet()) {
+			switch (attribute.getKey()) {
+				case "name":
+					this.name = attribute.getValue();
+					break;
+				case "password":
+					this.password = attribute.getValue();
+					break;
+				case "nickName":
+					this.nickName = attribute.getValue();
+					break;
+				case "description":
+					this.description = attribute.getValue();
+					break;
+				case "photoUrl":
+					this.photoUrl = attribute.getValue();
+					break;
+			}
+		}
+	}
+}
