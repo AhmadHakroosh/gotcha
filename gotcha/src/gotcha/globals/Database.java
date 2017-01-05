c319dd15b753fd51e9d0a3c22313db5632ce537e
Ahmad Hakroosh <ahmad@ahmad-h.com>
2017-01-05 18:02:17 +0200

    deleted Globals interface, and created Listener for DB creation upon server startup, and created Database class.

---
+++ gotcha/src/gotcha/globals/Database.java
@@ -0,0 +1,38 @@
+package gotcha.globals;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Database {
+	// Database parameters
+	private final String driver = "org.apache.derby.jdbc.EmbeddedDriver";
+	private final String dbName = "gotchaDB";
+	private final String protocol = "jdbc:derby:";
+	
+	private Connection connection = null;
+	
+	public Database () {
+		try {
+			Class.forName(driver);
+			this.connection = DriverManager.getConnection(protocol + dbName + ";create=true");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public Connection getConnection () {
+		return this.connection;
+	}
+	
+	public ResultSet execute (String query) throws SQLException {
+		Statement statement = connection.createStatement();
+		return statement.executeQuery(query);
+	}
+}
