6886bad052019904b27b3f58071574cee571525c
Ahmad Hakroosh <ahmad@ahmad-h.com>
2017-01-11 21:52:31 +0200

    little fixes

--- gotcha/src/gotcha/controller/Register.java
+++ gotcha/src/gotcha/controller/Register.java
@@ -2,8 +2,6 @@ package gotcha.controller;
 
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
