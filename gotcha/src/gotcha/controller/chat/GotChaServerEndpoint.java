278475c552f7167b40519bf3a0eced5144df2dc4
Ahmad Hakroosh <ahmad@ahmad-h.com>
2017-02-06 19:29:53 +0200

    little fix

--- gotcha/src/gotcha/controller/chat/GotChaServerEndpoint.java
+++ gotcha/src/gotcha/controller/chat/GotChaServerEndpoint.java
@@ -7,9 +7,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.Map;
 import java.util.Map.Entry;
 
 import javax.websocket.DecodeException;
